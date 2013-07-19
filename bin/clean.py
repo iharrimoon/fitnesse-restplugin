e6d7e46fdd4c1532a575dde32b84e43638024616
Stuart Sullivan <stuart.sullivan@uxpsystems.com>
2013-07-19 11:25:30 -0400

    clean up the response better

--- bin/clean.py
+++ bin/clean.py
@@ -1,10 +1,24 @@
 import os
 import subprocess
 import sys
+import re
+
+str = ""
 
 for line in sys.stdin:
   if "[java]" in line:
     line = line.replace("     [java] ", " ")
     line = line.strip()
-    print line
+    if line[0] == "."
+      line = line.replace(".", "TEST PASS - ", 1)
+    if line[0] == "F" 
+      line = line.replace("F", " 500 FAIL - ", 1)
+    if line[0] == "X"
+      line = line.replace("X", " 404 FAIL - ", 1)
+    line = line.repalace("FrontPage.TestRoot.TestingSuite.", "...", 1)
+    re.(r"R:\d+    ", "", line)
+    re.(r"E:\d+    ", "", line)
+    re.(r"I:\d+    ", "", line)
+      
+    
 sys.stdout.flush()
