862664238609ccb3e64a3729df4c90efdfbeeac3
Stuart Sullivan <stuart.sullivan@uxpsystems.com>
2013-07-19 11:27:46 -0400

    clean up the response better... but working

--- bin/clean.py
+++ bin/clean.py
@@ -9,11 +9,11 @@ for line in sys.stdin:
   if "[java]" in line:
     line = line.replace("     [java] ", " ")
     line = line.strip()
-    if line[0] == "."
+    if line[0] == ".":
       line = line.replace(".", "TEST PASS - ", 1)
-    if line[0] == "F" 
+    if line[0] == "F":
       line = line.replace("F", " 500 FAIL - ", 1)
-    if line[0] == "X"
+    if line[0] == "X":
       line = line.replace("X", " 404 FAIL - ", 1)
     line = line.repalace("FrontPage.TestRoot.TestingSuite.", "...", 1)
     re.(r"R:\d+    ", "", line)
