322192ce54627ffa8860f089a219f48885c5ecf8
Stuart Sullivan <stuart.sullivan@uxpsystems.com>
2013-07-29 15:48:49 -0400

    New tester

---
+++ FitNesse-RESTPlugin/src/test/java/com/test/api/SuiteTest.java
@@ -0,0 +1,28 @@
+package com.test.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import fitnesse.junit.FitNesseSuite;
+import fitnesse.junit.FitNesseSuite.FitnesseDir;
+import fitnesse.junit.FitNesseSuite.Name;
+import fitnesse.junit.FitNesseSuite.OutputDir;
+
+//DO NOT CHANGE THIS IS WHAT RUNS THE SUITE
+@RunWith(FitNesseSuite.class)
+
+//THE SUITE THAT YOU WOULD LIKE TO RUN
+@Name("FrontPage.TestRoot.TestingSuite")
+
+//THE LOCATION OF YOUR FITNESSE SUITES
+@FitnesseDir("../../fitnesseroot")
+
+//THE LOCATION WHERE THE RESULTS WILL BE STORED
+@OutputDir("TestResults")
+
+public class SuiteTest {
+	@Test
+	public void dummyTest() {
+		System.out.println("I work!");
+	}
+}
